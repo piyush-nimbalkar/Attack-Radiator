b90f8d1255212adc051c4ae6d4cb1dba8b0ecb71
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2013-11-12 19:26:20 -0500

    Added packet frequency to the parser and the json

--- parser.py
+++ parser.py
@@ -4,33 +4,36 @@ from collections import deque
 
 
 MAX_ENTRIES = 10
-memory_usage = deque([])
+packet_frequency = deque([])
 processor_time = deque([])
 io_operations = deque([])
+memory_usage = deque([])
 
 
 def average(seq):
     sum = 0
-    print(seq)
     for x in seq:
         sum = sum + x
     return sum / len(seq) if len(seq) > 0 else 0
 
 
 def remove_last_entry():
+    packet_frequency.popleft()
     processor_time.popleft()
     io_operations.popleft()
     memory_usage.popleft()
 
 
-def add_new_entry(value_one, value_two, value_three):
-    processor_time.append(value_one)
-    io_operations.append(value_two)
-    memory_usage.append(value_three)
+def add_new_entry(value_one, value_two, value_three, value_four):
+    packet_frequency.append(value_one)
+    processor_time.append(value_two)
+    io_operations.append(value_three)
+    memory_usage.append(value_four)
 
 
 def write_values():
     data = {}
+    data['packet_frequency'] = average(packet_frequency)
     data['processor_time'] = average(processor_time)
     data['io_operations'] = average(io_operations)
     data['memory_usage'] = average(memory_usage)
@@ -49,19 +52,18 @@ def tail(filename, n = 50):
 
 
 def main():
-
     while(1):
-        # log = tail("C:\PerfLogs\\Notepad.csv", MAX_ENTRIES) # for Windows
-        log = tail("data.csv", MAX_ENTRIES) # for development in Linux
+        log = tail("C:\PerfLogs\\notepad_log.csv", MAX_ENTRIES) # for Windows
+        # log = tail("notepad_log.csv", MAX_ENTRIES) # for development in Linux
         for entry in log:
             values = entry.split('"')
             if bool(values[3].strip()):
-                add_new_entry(float(values[3]), float(values[5]), float(values[7]))
+                add_new_entry(float(values[3]), float(values[5]), float(values[7]), float(values[9]))
             else:
-                add_new_entry(0, 0, 0)
+                add_new_entry(0, 0, 0, 0)
 
             write_values()
-            time.sleep(0.5)
+            time.sleep(1)
 
             if len(memory_usage) >= MAX_ENTRIES:
                 remove_last_entry()
